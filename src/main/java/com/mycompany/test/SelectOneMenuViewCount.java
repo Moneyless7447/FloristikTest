17e66254228eb62adf265a014f3cf832bedeb1e5
sumosascha <56337965+sumosascha@users.noreply.github.com>
2022-04-26 16:08:54 +0200

    menge als backingbean

---
+++ src/main/java/com/mycompany/test/SelectOneMenuViewCount.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
+import javax.inject.Named;
+/**
+ *
+ * @author Sascha Nickel
+ */
+@Named
+@RequestScoped
+public class SelectOneMenuViewCount {
+    
+    private String number; 
+    private List<SelectItem> numbers;
+    
+   @PostConstruct
+    public void init() {
+        
+        SelectItem g1 = new SelectItem(1);
+        SelectItem g2 = new SelectItem(2);   
+        SelectItem g3 = new SelectItem(3);
+        SelectItem g4 = new SelectItem(4);
+        SelectItem g5 = new SelectItem(5);
+        SelectItem g6 = new SelectItem(6);   
+        SelectItem g7 = new SelectItem(7);
+        SelectItem g8 = new SelectItem(8);
+        SelectItem g9 = new SelectItem(9);
+        SelectItem g10 = new SelectItem(10);
+
+
+        numbers = new ArrayList<SelectItem>();
+        numbers.add(g1);
+        numbers.add(g2);
+        numbers.add(g3);
+        numbers.add(g4);
+        numbers.add(g5);
+        numbers.add(g6);
+        numbers.add(g7);
+        numbers.add(g8);
+        numbers.add(g9);
+        numbers.add(g10);
+    }
+    
+    
+    
+    
+     public String getNumber() {
+        return number;
+    }
+ 
+    public void setNumber(String number) {
+        this.number = number;
+    }
+ 
+
+   
+    public List<SelectItem> getNumbers() {
+        return numbers;
+    }
+ 
+}
