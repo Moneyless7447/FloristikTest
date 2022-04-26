507154232d7e1be44158bd0b4c4c5b655f8069ac
sumosascha <56337965+sumosascha@users.noreply.github.com>
2022-04-26 09:49:50 +0200

    alle beans dateien erstellt

---
+++ src/main/java/com/mycompany/test/ServiceBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mycompany.test;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Sascha Nickel
+ */
+@Named(value = "serviceBean")
+@Dependent
+public class ServiceBean {
+
+    /**
+     * Creates a new instance of ServiceBean
+     */
+    public ServiceBean() {
+    }
+    
+}
---
+++ src/main/java/com/mycompany/test/ShoppingCartBean.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mycompany.test;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Sascha Nickel
+ */
+@Named(value = "shoppingCartBean")
+@Dependent
+public class ShoppingCartBean {
+
+    /**
+     * Creates a new instance of ShoppingCartBean
+     */
+    public ShoppingCartBean() {
+    }
+    
+}
