e272b7bd8a21b5306e27adfd41cfdd39ef8d4542
sumosascha <56337965+sumosascha@users.noreply.github.com>
2022-04-26 14:10:21 +0200

    Service hinzugefügt

--- src/main/java/com/mycompany/test/ServiceBean.java
+++ src/main/java/com/mycompany/test/ServiceBean.java
@@ -4,21 +4,39 @@
  */
 package com.mycompany.test;
 
+import java.util.List;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.RequestScoped;
+import com.mycompany.test.Service;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
 
 /**
  *
  * @author Sascha Nickel
  */
 @Named(value = "serviceBean")
-@Dependent
+@RequestScoped
 public class ServiceBean {
 
+    private ArrayList<Service> services;
     /**
-     * Creates a new instance of ServiceBean
+     * Creates a new instance of ProductBean
      */
     public ServiceBean() {
+
     }
+    @PostConstruct  
+    public void init() {
+        Service service1 = new Service("Hochzeitspaket",3.0f);
+        Service service2 = new Service("Friedhofbegrünung",5.0f);
+        services = new ArrayList<>();
+        services.add(service1);
+        services.add(service2);
+    }
+    public List<Service> getServices() {
+        return services;
+    }
+    
     
 }
